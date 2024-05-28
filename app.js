d7229ca197dfe6ee79ea160758ccbeafb54999f2
Eduard-R <edi_r@gmx.de>
2024-05-28 23:21:08 +0200

    "modified Pong game logic and UI, and added stylesheet to HTML file."

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 
-const racketSpeed = 5;
-let player1coordY = 200; 
-let player2coordY = 200;
+const racketSpeed = 20;
+let player1coordY = 320; 
+let player2coordY = 320;
 let key = {};
 let ball = {
-    coordX: 350,
-    coordY: 230,
-    speedX: 3,
+    coordX: 640,
+    coordY: 355,
+    speedX: 10,
     speedY: 0,
 };
+let player1Score = 0;
+let player2Score = 0;
 
 document.addEventListener('keydown', e => key[e.keyCode] = true);
 document.addEventListener('keyup', e => key[e.keyCode] = false);
@@ -20,11 +22,14 @@ setInterval(loop, 1000 / 60);
 
 function draw() {
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, 720, 480);
+    ctx.fillRect(0, 0, 1280, 720);
     ctx.fillStyle = 'white';
     ctx.fillRect(10, player1coordY, 10, 80);
-    ctx.fillRect(700, player2coordY, 10, 80);
+    ctx.fillRect(1260, player2coordY, 10, 80);
     ctx.fillRect(ball.coordX, ball.coordY, 10, 10);
+    ctx.font = "50px monospace";
+    ctx.fillText(player1Score, 100, 60)
+    ctx.fillText(player2Score, 1125, 60)
     requestAnimationFrame(draw);
 }
 
@@ -45,28 +50,41 @@ function loop() {
     ball.coordX = ball.coordX + ball.speedX;
     ball.coordY = ball.coordY + ball.speedY;
 
-    if (ball.coordX < 20 || ball.coordX > 690) {
+    if (ball.coordX < 20 || ball.coordX > 1250) {
+        // Player 1
         if (ball.coordY > player1coordY && ball.coordY < player1coordY + 80 && ball.speedX < 0) {
             ball.speedX = -ball.speedX;
-            ball.speedY = (ball.coordY - player1coordY - 40) * 0.1;
+            ball.speedY = (ball.coordY - player1coordY - 60) * 0.1;
         }
-
+        // Player 2
         if (ball.coordY > player2coordY && ball.coordY < player2coordY + 80 && ball.speedX > 0) {
             ball.speedX = -ball.speedX;
-            ball.speedY = (ball.coordY - player2coordY - 40) * 0.1;
+            ball.speedY = (ball.coordY - player2coordY - 60) * 0.1;
         }
     }
 
-    if (ball.coordY < 0 || ball.coordY > 470) {
+    if (ball.coordY < 0 || ball.coordY > 710) {
         ball.speedY = -ball.speedY;
     }
 
-    if (ball.coordX < 0 || ball.coordX > 720) {
+    if (ball.coordX < 0) {
+        player2Score++;
         ball = {
-            coordX: 350,
-            coordY: 230,
-            speedX: 3,
+            coordX: 640,
+            coordY: 355,
+            speedX: 10,
             speedY: 0
         }
     }
+    if (ball.coordX > 1280) {
+        player1Score++;
+        ball = {
+            coordX: 640,
+            coordY: 355,
+            speedX: 10,
+            speedY: 0
+        }
+    }
+    console.log(ball.speedX);
+    console.log(ball.coordX);
 }
